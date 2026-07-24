e94b397a113bbfa4d5edf31177c8f9ab645c8941
gusenga thierry <t.gusenga@alustudent.com>
2026-07-24 08:39:00 +0200

    Add Enemy that reports death through events

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+// An enemy: reuses Health, reports its death through GameEvents
+[RequireComponent(typeof(Health))]
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int defense = 0;
+    [SerializeField] private float deathDelay = 0.2f;
+
+    public int Defense => defense;
+
+    private Health health;
+    private bool dead;
+
+    void Awake()
+    {
+        health = GetComponent<Health>();
+        health.OnDied += Die;
+    }
+
+    void OnDestroy()
+    {
+        if (health != null) health.OnDied -= Die;
+    }
+
+    void Die()
+    {
+        if (dead) return;
+        dead = true;
+        RunStats.EnemiesDefeated++;
+        GameEvents.RaiseEnemyDefeated();
+        foreach (var col in GetComponentsInChildren<Collider>()) col.enabled = false;
+        Destroy(gameObject, deathDelay);
+    }
+}
